9d6d826150e62180987d5c04a6f1af46a622bbff
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-08-23 10:31:29 +0200

    tester ut forskjellige ting i 1a/b

---
+++ kap 1/intro.py
@@ -0,0 +1,12 @@
+# tall = input("skriv et tall")
+# tall = float(tall)
+# differanse = 7 - tall
+
+# if tall <7:
+#     print(f"Tallet {tall:.2f} er {differanse: .2f} mindre enn 7")
+
+tall = input("Skriv et tall")
+tall = float(tall)
+
+if tall > 50 and tall < 100:
+    print("tallet er mellom 50 og 100")
