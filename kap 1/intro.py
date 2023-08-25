62789a8388d9ba2db7c4481ed661e5e780b76526
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-08-25 10:23:40 +0200

    Jobbet med 1b

--- kap 1/intro.py
+++ kap 1/intro.py
@@ -5,8 +5,42 @@
 # if tall <7:
 #     print(f"Tallet {tall:.2f} er {differanse: .2f} mindre enn 7")
 
-tall = input("Skriv et tall")
-tall = float(tall)
+# tall = input("Skriv et tall")
+# tall = float(tall)
 
-if tall > 50 and tall < 100:
-    print("tallet er mellom 50 og 100")
+
+
+# if tall > 50 and tall < 100:
+#     print("tallet er mellom 50 og 100")
+
+# else: 
+#     print("Tallet er ikke mellom 50 og 100")
+
+# a = int(input("skriv et tall"))
+# b = int(input("skriv et tall"))
+
+
+
+# if a == 0 and b == 0:
+#     print("begge tallene er lik null")
+# else:
+#     print("begge tallene er ikke lik null")
+
+# poeng = int(input("skriv inn din poengsum fra 1-100"))
+
+# if poeng <= 100 and poeng >= 0:
+#     if poeng >= 0 and poeng <= 20:
+#         print("du får karakteren 1")
+#     elif poeng >=21 and poeng <=40:
+#         print("du får karakteren 2")
+#     elif poeng >= 41 and poeng <=60:
+#         print("du får karakteren 3")
+#     elif poeng >= 61 and poeng <=80:
+#         print("du får karakteren 4")
+#     elif poeng >= 81 and poeng <=90:
+#         print("du får karakteren 5")
+#     elif poeng >= 91 and poeng <=100:
+#         print("du får karakteren 6")
+# else:
+#     print("du må skrive inn et tall mellom 0 og 100")
+
