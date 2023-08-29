a5368a0e7e22586b9e70575f7b8f278e8314e13d
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-08-29 15:46:18 +0200

    nesten ferdig stein saks og papir

---
+++ Stein saks pose.py
@@ -0,0 +1,43 @@
+import random as r
+tall = 0
+streak = 0
+
+
+
+
+
+
+while streak < 3:
+    
+    poeng = 0
+    streak = 0
+    tall = int(r.randint(1,3))
+    brukerInp = input("Skriv 'stein' 'saks' eller 'papir'")
+    if brukerInp == "stein":
+        poeng = 1
+    elif brukerInp == "saks":
+        poeng = 2
+    elif brukerInp == "papir":
+        poeng = 3
+
+    print(poeng)
+    print(tall)
+
+    if poeng == 1 and tall == 2:
+        print("du vant")
+        streak = streak + 1
+    elif poeng == 2 and tall == 3:
+        print("du vant")
+        streak = streak + 1
+    elif poeng == 3 and tall == 1:
+        print("du vant")
+        streak = streak + 1
+    elif poeng == tall:
+        print("uavgjort")
+    else:
+        print("datamaskinen vant")
+
+if streak == 3:
+        print("du vant tre ganger")
+    
+    
