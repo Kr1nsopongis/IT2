156ecf9440597efd4c5d9018c147ae3b9c79715a
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-09-05 13:52:20 +0200

    lager vær logging

--- kap 2/Testing.py
+++ kap 2/Testing.py
@@ -10,13 +10,22 @@ nedbør = []
 temperatur = []
 vindstyrke = []
 
-for i in range (1,8):
-    regn = input("Hvor mange millimeter nedbør var det denne dagen")
-    temp = input("Hva var temperaturen denne dagen?")
-    vind = input("Hva var vindstyrken denne dagen?")
+for i in range (0,7):
+    regn = float(input (f"Hvor mange millimeter nedbør var det på på {dager[i]}?\t:"))
+    temp = float(input(f"Hva var temperaturen på {dager[i]}?\t\t\t:"))
+    vind = float(input(f"Hva var vindstyrken på {dager[i]}?\t\t\t\t:"))
     nedbør.append(regn)
     temperatur.append(temp)
     vindstyrke.append(vind)
     print(nedbør)
     print(temperatur)
     print(vindstyrke)
+
+høyestRegn = max(nedbør)
+print(høyestRegn)
+
+print("På",dager[nedbør.index(høyestRegn)],"var det høyest mengde nedbør med",høyestRegn,"mm på 24 timer!")
+print("Det regnet i gjennomsnitt: ",sum(nedbør)/len(nedbør),"mm hver dag")
+print("Det var i gjennomsnitt: ",sum(temperatur)/len(temperatur),"grader celcius")
+print("Det blåste i gjennomsnitt: ",sum(vindstyrke)/len(vindstyrke),"m/s hver dag")
+
