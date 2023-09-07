0d948c09206a4718df104db7c0cd1eb55f6c7f0f
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-09-07 16:41:23 +0200

    Litt forskjellig testing med lister

---
+++ kap 1/ordbøker.py
@@ -0,0 +1,15 @@
+# person = {
+#     "fornavn": "Marius",
+#     "etternavn": "Rotseth"  
+# }
+
+# print("Hei!, jeg heter",person["fornavn"],person["etternavn"])
+
+
+vær = {}
+
+vær["maksTemp"] = "7 grader celsius"
+vær["minTemp"] = "4 grader celsius"
+vær["nedbør"] = "3 mm nedbør"
+
+print(vær["maksTemp"],vær["minTemp"],vær["nedbør"])
