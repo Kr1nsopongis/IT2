ebc41523ecf2544dacc8f9a7251ec4129851fe76
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-09-22 09:23:56 +0200

    funskjoner med oppgavesamling

--- kap 1/funksjoner.py
+++ kap 1/funksjoner.py
@@ -1,12 +1,30 @@
-import math 
+omgjøringsverdi = 3.6
+fart = 0
+strekning = 0
+tid = 0
 
-# radius = input("Skriv radussen på kulissen")
-# formel = 4 * math.pi * int(radius) * 2
+def main():
+    strekning = input("Skriv inn hvor lang strekningen er")
+    global tid
+    tid = input("Hvor lang tid du på strekningen i sekunder")
+    print(tid)
+    speedFromTime()
+    speedCheck()
 
-# def regne():
-#     print(f"{round(formel,2)}")
+def speedFromTime(): 
+    global fart
+    print(tid)
+    fart = float(strekning)/float(tid)
+    print("du kjørte med en gjennomsnitlig fart på",fart,"meter i sekundet")
+def speedCheck(): 
+    global fart
+    fart = fart/omgjøringsverdi
+    if fart <= 80: 
+        print(" du kjørte i 80 eller mer km/h")
+    else:
+        print("du kjørte ikke i 80 km/h")
 
-# regne()
 
-def alkoholpris ():
-    pris = input
+
+main()
+
