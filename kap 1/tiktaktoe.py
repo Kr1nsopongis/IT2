21782a24479347174dc7e0c648696d5bfff2f6c4
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-10-01 16:38:54 +0200

    Update tiktaktoe.py

--- kap 1/tiktaktoe.py
+++ kap 1/tiktaktoe.py
@@ -6,7 +6,7 @@ plassVerdi = {
     "1" : ["VN","-"],
     "2" : ["MN","-"],
     "3" : ["HN","-"],
-    "11" : ["HM","-"],
+    "11" : ["VM","-"],
     "12" : ["MM","_"],
     "13" : ["HM","-"],
     "21" : ["HO","-"],
@@ -16,3 +16,9 @@ plassVerdi = {
 print("Velkommen til tik tak toe!")
 print(f"Spillebrettet:\n{plassVerdi['23'][1]} {plassVerdi['22'][1]} {plassVerdi['21'][1]}\n{plassVerdi['13'][1]} {plassVerdi['12'][1]} {plassVerdi['11'][1]}\n{plassVerdi['3'][1]} {plassVerdi['2'][1]} {plassVerdi['1'][1]}")
 
+while True:
+    if keyboard.is_pressed("left arrow"):
+        startVerdi = startVerdi -1
+    if keyboard.is_pressed("left arrow"):
+    if keyboard.is_pressed("left arrow"):
+    if keyboard.is_pressed("left arrow"):
