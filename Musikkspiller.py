c361f734332a55e42141e48c10b1ccbdaf13f1a0
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-10-27 18:09:21 +0200

    fant ut av det med å se på v1 eksempelet

--- Musikkspiller.py
+++ Musikkspiller.py
@@ -6,9 +6,12 @@ class Musikk():
         self.navn = navn
         self.filsti = filsti
 
-sang1 = Musikk("sang1","musikkspiller/horror-hit-logo-142395.mp3")
+sang1 = Musikk("sang1","lyd/horror.mp3")
 
 pygame.mixer.init()
 pygame.mixer.music.set_volume(0.7) 
 pygame.mixer.music.load(sang1.filsti)
 pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy() ==True:
+    pass
