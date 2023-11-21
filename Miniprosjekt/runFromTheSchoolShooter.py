018a821ce3fb69ed94635ab78ba6298498a692fb
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-11-21 22:31:30 +0100

    Update runFromTheSchoolShooter.py

--- Miniprosjekt/runFromTheSchoolShooter.py
+++ Miniprosjekt/runFromTheSchoolShooter.py
@@ -1,15 +1,21 @@
 import pygame
 
 class Player(): 
-    def __init__(self,positionX,positionY):
+    def __init__(self,positionX,positionY,hp):
         self.positionX = positionX
         self.positionY = positionY
+        self.hp = hp 
 
 class Maps():
     def __init__(self,mapPositionX,mapPositionY,effekt,timer):
         self.mapPositionX = mapPositionX
         self.mapPositionY = mapPositionY
         self.effekt = effekt
+        self.timer = timer
 
-room1 = Maps(1,1,"none")
+player = Player(0,0,100)
+
+room1 = Maps(1,1,"none",0)
 room2 = Maps(2,1,"trap",5)
+
+print(player.positionX,player.positionY)
