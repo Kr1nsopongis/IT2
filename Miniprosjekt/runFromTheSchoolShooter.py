bda01d1bebb4d83f94d091172054eb37f39fe603
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-11-23 09:24:00 +0100

    Update runFromTheSchoolShooter.py

--- Miniprosjekt/runFromTheSchoolShooter.py
+++ Miniprosjekt/runFromTheSchoolShooter.py
@@ -1,10 +1,12 @@
 import pygame
+import random 
 
 class Player(): 
-    def __init__(self,positionX,positionY,hp):
+    def __init__(self,positionX,positionY,hp,weaponDmg):
         self.positionX = positionX
         self.positionY = positionY
         self.hp = hp 
+        self.weapon = weaponDmg
 
 class Maps():
     def __init__(self,mapPositionX,mapPositionY,effekt,timer):
@@ -13,9 +15,35 @@ class Maps():
         self.effekt = effekt
         self.timer = timer
 
-player = Player(0,0,100)
 
+
+def movement():
+    
+
+    for i in range(0,random.randint(0,3)):
+        retningValg.append(retninger[random.randint(0,len(retninger)-1)])
+    
+    print(retningValg)
+
+movement()
+
+
+
+
+
+
+    
+
+
+
+# Players
+player = Player(0,0,100,0)
+Boss = Player(random.randint(0,10),random.randint(0,10),200,0)
+
+# Rooms
+startRoom = Maps(0,0,"weaponSelect",0)
 room1 = Maps(1,1,"none",0)
 room2 = Maps(2,1,"trap",5)
 
 print(player.positionX,player.positionY)
+
