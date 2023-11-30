97ac29bd7942bc71cd29fa6294d86c83406204dd
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-11-30 22:28:58 +0100

    Lagd ferdig kartet, forbedret litt diverse

--- Miniprosjekt/LabyrintSpill.py
+++ Miniprosjekt/LabyrintSpill.py
@@ -15,8 +15,9 @@ class Maps():
         self.retninger = retninger
     
 class SpecialMaps(Maps):
-    def __init__(self, mapPosition, retninger):
+    def __init__(self, mapPosition, retninger,effekt):
         super().__init__(mapPosition, retninger)
+        self.effekt 
         
 
 # Players
@@ -29,9 +30,26 @@ boss = Player([0,1],200,0)
 rom = ["startRoom","room01","room11"]
 
 #[opp,ned,høyre,venstre]
-startRoom = Maps([0,0],[0,1,2,3])
-room01 = Maps([0,1],[3,4])
-room11 = Maps([1,1],[4])
+roomN1N1 = SpecialMaps([-1,-1],[0],"trap")
+room0N1 = Maps([0,-1],[0,2])
+room1N1 = Maps([1,-1],[1,2,3])
+room2N2 = Maps([2,-1],[0,3])
+
+roomN10 = Maps([-1,0],[0,1,2])
+startRoom = SpecialMaps([0,0],[0,1,2,3],"weaponSelect")
+room10 = Maps([1,0],[0,1,3])
+room20 = Maps([2,0],[0,1])
+
+roomN11 = Maps ([-1,1],[0,1,2])
+room01 = Maps ([0,1],[1,2,3])
+room11 = Maps ([1,1],[1,3])
+room21 = Maps ([2,1],[0,1])
+
+roomN12 = Maps ([-1,2],[1,2])
+room02 = Maps ([0,2],[2,3])
+room12 = SpecialMaps ([1,2],[2,3],"trap")
+room22 = Maps ([2,2],[0,1])
+
 
 def start():
     print(Start)
@@ -58,7 +76,7 @@ def movement():
                     if int(retningInput) == retningvalg[i]:
                         validInput = True          
                 except:
-                    print("feil")
+                    print("")
 
         retningInput = int(retningInput)
             
@@ -85,21 +103,38 @@ def fight():
         boss.hp -= 10
         return boss.hp
     
-    print("Ånei!!!! Der var det en Boss!!!! \n\nDine valg er: \n\n\t1 = Heal \n\t2 = Angrip")
-    svar = int(input("Hva velger du?"))
+    validInput = False 
+
+    while validInput == False:
+        print("\n\n\n\nÅnei!!!! Der var det en Boss!!!! \n\nDine valg er: \n\n\t1 = Heal \n\t2 = Angrip")
+        svar = input("Hva velger du?")
+
+        alternativer = [1,2]
+       
+        for i in range(0,len(alternativer)):
+                try:
+                    if int(svar) == alternativer[i]:
+                        validInput = True          
+                except:
+                    print("")
 
     if svar == 1:
         heal()
+        print("Din gjenværende HP: ",player.hp)
     elif svar ==2:
         angrip()
+        print("Bossens gjenværende HP: ",boss.hp)
 
-    
+while player.hp >= 0 and boss.hp >= 0:
+
+    if player.positon == [0,0]:
+           
 
         
 
 # Intro
 
 # start()
-movement()
-# fight()
+# movement()
+fight()
 
