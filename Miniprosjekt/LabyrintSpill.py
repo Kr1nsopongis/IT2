0300ea64a5045e0c195c0a9b1cdf7f54f492ccbc
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2023-12-05 13:56:28 +0100

    fikser oop prosjektet og gjør det mer oopete

--- Miniprosjekt/LabyrintSpill.py
+++ Miniprosjekt/LabyrintSpill.py
@@ -3,67 +3,14 @@ import random
 from ascii import *
 
 class Player(): 
-    def __init__(self,positionP,hp,weaponDmg):
-        self.position = positionP
+    def __init__(self,name,position,hp,weaponDmg):
+        self.name = name
+        self.position = position
         self.hp = hp 
         self.weapon = weaponDmg
 
-
-class Maps():
-    def __init__(self,mapPosition,retninger):
-        self.mapPosition = mapPosition
-        self.retninger = retninger
-    
-class SpecialMaps(Maps):
-    def __init__(self, mapPosition, retninger,effekt):
-        super().__init__(mapPosition, retninger)
-        self.effekt 
-        
-
-# Players
-player = Player([0,0],100,0)
-boss = Player([0,1],200,0)
-
-# random.randint(0,10),random.randint(0,10)
-
-# Rooms
-rom = ["startRoom","room01","room11"]
-
-#[opp,ned,høyre,venstre]
-roomN1N1 = SpecialMaps([-1,-1],[0],"trap")
-room0N1 = Maps([0,-1],[0,2])
-room1N1 = Maps([1,-1],[1,2,3])
-room2N2 = Maps([2,-1],[0,3])
-
-roomN10 = Maps([-1,0],[0,1,2])
-startRoom = SpecialMaps([0,0],[0,1,2,3],"weaponSelect")
-room10 = Maps([1,0],[0,1,3])
-room20 = Maps([2,0],[0,1])
-
-roomN11 = Maps ([-1,1],[0,1,2])
-room01 = Maps ([0,1],[1,2,3])
-room11 = Maps ([1,1],[1,3])
-room21 = Maps ([2,1],[0,1])
-
-roomN12 = Maps ([-1,2],[1,2])
-room02 = Maps ([0,2],[2,3])
-room12 = SpecialMaps ([1,2],[2,3],"trap")
-room22 = Maps ([2,2],[0,1])
-
-
-def start():
-    print(Start)
-
-
-def movement():
-        retningvalg = []
-        romRN = ""
-
-        for i in range(0,len(rom)-1):
-            if eval(rom[i]).mapPosition == player.position :
-                retningvalg = eval(rom[i]).retninger
-                romRN = rom[i]
-        
+    def movement(self):
+        global retningvalg
         validInput = False
 
 
@@ -81,60 +28,150 @@ def movement():
         retningInput = int(retningInput)
             
         if retningInput == 0:
-            player.position[1] += 1
+            self.position[1] += 1
         elif retningInput == 1:
-            player.position[1] -= 1
+            self.position[1] -= 1
         elif retningInput == 2:
-            player.position[0] += 1
+            self.position[0] += 1
         elif retningInput == 3:
-            player.position[0] -= 1
+            self.position[0] -= 1
  
 
-        print(player.position)
+        print(self.position)
 
-        return player.position
-
-def fight():
-    def heal():
-        player.hp += 10
-        return player.hp
+        return self.position
     
-    def angrip():
-        boss.hp -= 10
-        return boss.hp
-    
-    validInput = False 
-
-    while validInput == False:
-        print("\n\n\n\nÅnei!!!! Der var det en Boss!!!! \n\nDine valg er: \n\n\t1 = Heal \n\t2 = Angrip")
-        svar = input("Hva velger du?")
-
-        alternativer = [1,2]
-       
-        for i in range(0,len(alternativer)):
-                try:
-                    if int(svar) == alternativer[i]:
-                        validInput = True          
-                except:
-                    print("")
-
-    if svar == 1:
-        heal()
-        print("Din gjenværende HP: ",player.hp)
-    elif svar ==2:
-        angrip()
-        print("Bossens gjenværende HP: ",boss.hp)
-
-while player.hp >= 0 and boss.hp >= 0:
-
-    if player.positon == [0,0]:
-           
-
+    def fight(self,annen):
+        def heal():
+            self.hp += 10
+            if self.hp > 100:
+                self.hp = 100
+            print(f"Du har nå {self.hp} liv igjen")
+            return self.hp
+            
         
+        def angrip():
+            annen.hp -= self.weapon
+            print(f"{annen} har nå {annen.hp} igjen")
+            return annen.hp
+        
+        validInput = False 
+
+        while validInput == False:
+            print("\n\n\n\nÅnei!!!! Der var det en Boss!!!! \n\nDine valg er: \n\n\t1 = Heal \n\t2 = Angrip")
+            svar = input("Hva velger du?")
+
+            alternativer = [1,2]
+        
+            for i in range(0,len(alternativer)):
+                    try:
+                        if int(svar) == alternativer[i]:
+                            svar = int(svar)
+                            validInput = True          
+                    except:
+                        print("")
+        if svar == 1:
+            heal()
+        elif svar == 2:
+            angrip()
+
+    def weaponSelect(self):
+        weapon = input("Du kan velge mellom Øks(0) og Sverd(1) hvis ikke har du bare hender")
+        weapons = [10,15]
+        try:
+            self.weapon = weapons[int(weapon)]
+        except:
+            print("Du har bare henda å sloss med, Du kan komme tilbake til dette rommet hvis det skulle funke dårlig!")
+
+
+class Maps():
+    def __init__(self,mapPosition,retninger):
+        self.mapPosition = mapPosition
+        self.retninger = retninger
+    
+class SpecialMaps(Maps):
+    def __init__(self, mapPosition, retninger,effekt):
+        super().__init__(mapPosition, retninger)
+        self.effekt = effekt
+        
+
+# Players
+player = Player("Spiller",[0,0],100,10)
+boss = Player("Boss",[2,0],200,10)
+
+# random.randint(0,10),random.randint(0,10)
+
+# Rooms
+rom = [
+       "roomN1N1","room0N1","room1N1","room2N1",
+       "roomN10","startRoom","room10","room20",
+       "roomN11","room01","room11","room21",
+       "roomN12","room02","room12","room22"
+       ]
+
+#[opp,ned,høyre,venstre]
+roomN1N1 = SpecialMaps([-1,-1],[0],"trap")
+room0N1 = Maps([0,-1],[0,2])
+room1N1 = Maps([1,-1],[0,2,3])
+room2N1 = Maps([2,-1],[0,3])
+
+roomN10 = Maps([-1,0],[0,1,2])
+startRoom = SpecialMaps([0,0],[0,1,2,3],"weaponSelect")
+room10 = Maps([1,0],[0,1,3])
+room20 = Maps([2,0],[0,1])
+
+roomN11 = Maps ([-1,1],[0,1,2])
+room01 = Maps ([0,1],[1,2,3])
+room11 = Maps ([1,1],[1,3])
+room21 = Maps ([2,1],[0,1])
+
+roomN12 = Maps ([-1,2],[1,2])
+room02 = Maps ([0,2],[2,3])
+room12 = SpecialMaps ([1,2],[2,3],"finish")
+room22 = SpecialMaps([2,2],[1,3],"test")
+
+
+def start():
+    print(Start)
+
+
+
 
 # Intro
 
-# start()
-# movement()
-fight()
+# player.movement()
 
+# start()
+
+while boss.hp > 0 and player.hp > 0:
+    if player.position == boss.position:
+        player.fight(boss) 
+        break
+    
+    
+    retningvalg = []
+    romRN = ""
+    effekt = ""
+
+    for i in range(0,len(rom)):
+        if eval(rom[i]).mapPosition == player.position :
+            retningvalg = eval(rom[i]).retninger
+            romRN = rom[i]
+            print(romRN)
+            try:
+                effekt = eval(romRN).effekt
+            except:
+                effekt = ""
+    print(effekt)        
+
+    if effekt == "weaponSelect":
+        print("test")
+        player.weaponSelect()
+    
+    player.movement()
+
+    
+                
+
+     
+    
