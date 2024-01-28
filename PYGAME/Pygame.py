b55d373d19417f1865963b779bcb13c5a898bc0e
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2024-01-28 17:09:25 +0100

    nesten ferdig med første spillebrett

--- PYGAME/Pygame.py
+++ PYGAME/Pygame.py
@@ -3,6 +3,8 @@ import math as m
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_w, K_a,K_s,K_d)
 import os
 
+pygame.init()
+
 def absRef(relRef):
    return os.path.join(os.path.dirname(__file__), relRef) #Stjelt av linus
 
@@ -18,14 +20,26 @@ WHITE = (255, 255, 255)
 
 background = GRAY
 clock = pygame.time.Clock()
-bakgrunn = pygame.image.load(absRef("Gulv.jpg")) 
+bakgrunn = pygame.image.load(absRef("Gulv.jpg"))
+
+stjerne = pygame.image.load(absRef("stjernee.png")) 
+stjerne = pygame.transform.scale(stjerne, (60,60))
+
+
+font = pygame.font.SysFont("Arial",30, bold = True)
 
 screen = pygame.display.set_mode((800, 800))
 
-boks = pygame.draw.circle(screen, (255, 0, 0), (100, 250), 50)
+
+def SkrivTekst(tekst,font,tekstFarge,x,y):
+   tekstBoks = font.render(tekst,True, tekstFarge)
+   screen.blit(tekstBoks,(x,y))
+
+def printBilde(bilde,x,y):
+   screen.blit(bilde,(x,y))
 
 
-pygame.init()
+
 
 class Ball:
   """Klasse for å representere en ball"""
@@ -42,15 +56,7 @@ class Ball:
     """Metode for å tegne ballen"""
     pygame.draw.circle(self.vindusobjekt, (255, 69, 0), (self.x, self.y), self.radius) 
 
-  # def flytt(self):
-  #   """Metode for å flytte ballen"""
-  #   # Sjekker om ballen er utenfor høyre/venstre kant
-  #   if ((self.x - self.radius) <= 0) or ((self.x + self.radius) >= self.vindusobjekt.get_width()):
-  #     self.xFart = -self.xFart
-  #     print(self.xFart)
 
-  #   if ((self.y - self.radius) <= 0) or ((self.y + self.radius) >= self.vindusobjekt.get_height()):
-  #     self.yFart = -self.yFart
 
   def flytt(self, taster):
     if taster[K_UP] and self.y > 0+self.radius/2:
@@ -65,18 +71,26 @@ class Ball:
     
       
 class Ball2(Ball):
-   def __init__(self, x, y, xFart, yFart, radius, vindusobjekt):
-      super().__init__(x, y, xFart, yFart, radius, vindusobjekt)
-      self.x = x
-      self.y = y
-      self.xFart = xFart
-      self.yFart = yFart
-      self.radius = radius
-      self.vindusobjekt = vindusobjekt
+  def __init__(self, x, y, xFart, yFart, radius, vindusobjekt):
+    super().__init__(x, y, xFart, yFart, radius, vindusobjekt)
+    self.x = x
+    self.y = y
+    self.xFart = xFart
+    self.yFart = yFart
+    self.radius = radius
+    self.vindusobjekt = vindusobjekt
+
+  def flytt(self):
+    if ((self.x - self.radius) <= 0) or ((self.x + self.radius) >= self.vindusobjekt.get_width()):
+      self.xFart = -self.xFart
+      print(self.xFart) 
+
+    if ((self.y - self.radius) <= 0) or ((self.y + self.radius) >= self.vindusobjekt.get_height()):
+      self.yFart = -self.yFart
     
-      def fart(self): 
-          self.x += self.xFart
-          self.y += self.yFart
+  def fart(self): 
+    self.x += self.xFart
+    self.y += self.yFart
   #  def flytt(self, taster):
   #   if taster[K_w] and self.y > 0+self.radius/2:
   #     self.y -= self.yFart
@@ -90,8 +104,10 @@ class Ball2(Ball):
     
 
 # Lager et Ball-objekt
-ball = Ball(350, 250, 4, 4 , 20, screen)
-ball2 = Ball2(100, 250, 4, 4, 20, screen)
+ball = Ball(400, 750, 4, 4 , 20, screen)
+ball2 = Ball2(100, 250, 4, 0, 20, screen)
+ball3 = Ball2(100, 300, 7, 0, 20, screen)
+ball4 = Ball2(100, 350, 6.3, 0, 20, screen)
 
 
 
@@ -115,6 +131,8 @@ while fortsett:
 
     screen.fill(background)
     screen.blit(bakgrunn, (0,0))
+    
+    
 
     # def finnAvstand(ball, ball2):
     #     xAvstand2 = (ball.x - ball2.x)**2  # x-avstand i andre
@@ -140,9 +158,25 @@ while fortsett:
     # finnAvstand(ball, ball2)
 
     ball.flytt(trykkede_taster)
+    ball2.flytt()
     ball2.fart()
+    ball3.flytt()
+    ball3.fart()
+    ball3.tegn()
+    ball4.flytt()
+    ball4.fart()
+    ball4.tegn()
     ball.tegn()
     ball2.tegn()
+
+    printBilde(stjerne,370,30)
+    
+
+    pygame.draw.rect(screen, (133,133,133), pygame.Rect(0, 0, 200, 600))
+    pygame.draw.rect(screen, (133,133,133), pygame.Rect(600,0, 200, 600))
+    SkrivTekst("Gå til stjernen!",font,(0,0,0), 10,100)
+    printBilde(stjerne,66,200)
+
 
     pygame.display.update()
     clock.tick(60)
