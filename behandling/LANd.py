597ec2994c66418fc3ddb6455993115c65e89dc6
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2024-02-16 09:07:22 +0100

    Fortsetter og tester

--- behandling/LANd.py
+++ behandling/LANd.py
@@ -1,10 +1,15 @@
-import json
+# import json
 
-f = open("behandling/countries.json", "r") # OBS: Spør om du får feilmelding her
-land = json.loads(f.read())
+# f = open("behandling/countries.json", "r") # OBS: Spør om du får feilmelding her
+# land = json.loads(f.read())
 
-# Iterer gjennom alle landene i landet
-for l in land:
-    print(l["name"]["common"])
-    print(l["region"])
+# # Iterer gjennom alle landene i landet
+# for l in land:
+#     print(l["name"]["common"])
+#     print(l["region"])
     
+
+liste = [1,3]
+
+if liste.index(3) == -1:
+    print("hello")
