3814fd7b6699aa04204c1efdc4741533c78746c3
Kr1nsopongis <112948685+Kr1nsopongis@users.noreply.github.com>
2024-06-10 00:49:40 +0200

    Fortsetter å utvide programmet. Mindre kluss nå

--- MUNTLIG EKSAMEN!!/main.py
+++ MUNTLIG EKSAMEN!!/main.py
@@ -33,14 +33,16 @@ class RadonStation(WeatherStation):
       print("Information: ","\n\tStation name: ",self.name,"\n\tRadon level: ",self.radonLevel)
     
 
-with open(dataFrame, encoding="utf-8-sig") as file: 
-  dataFrame = csv.reader(file, delimiter=",")
+with open (absRef("weatherStations.csv"), 'r', newline='',encoding= 'utf-8') as file:
+  reader = csv.reader(file, delimiter=",")
 #   writer = csv.writer(file)
-  for row in dataFrame: # Retreives stations from CSV and adds to list 
-#     if row[3] != "NaN":
-#        stations.append(RadonStation(row[0],row[1],row[2],row[3]))
-#     else:
-    stations.append(WeatherStation(row[0],row[1],row[2]))
+  for row in reader: # Retreives stations from CSV and adds to list 
+    print(" ".join(row))
+ 
+    if row[3] != "NaN":
+      stations.append(RadonStation(row[0],row[1],row[2],row[3]))
+    else:
+      stations.append(WeatherStation(row[0],row[1],row[2]))
 
   
 def updateDF():
@@ -49,7 +51,12 @@ def updateDF():
     writer.writerow(['name','positionX','positionY','radonLevel','pressure','moisture','wind'])   
 
     for i in range(1,len(stations)):
-        writer.writerow([stations[i].name,stations[i].positionX,stations[i].positionY])   
+      try:
+        if stations[i].radonLevel != "NaN":
+          writer.writerow([stations[i].name,stations[i].positionX,stations[i].positionY,stations[i].radonLevel])
+      except:
+        writer.writerow([stations[i].name,stations[i].positionX,stations[i].positionY,])
+              
 
 def addStation():
     print("Fill in information about station. If no info put in: NaN")
@@ -57,11 +64,22 @@ def addStation():
     positionX = input("Position X?")
     positionY = input("Position Y?")
     radonLevel = input("Radon level?")
-    stations.append(WeatherStation(name,positionX,positionY))
+    if radonLevel != "NaN":
+      stations.append(RadonStation(name,positionX,positionY,radonLevel))
+    else:
+      stations.append(WeatherStation(name,positionX,positionY))
 
-print(stations[i].selfPos())
-print(stations[1].positionX)
-addStation()
-updateDF()
+# print(stations[i].selfPos())
+# print(stations[1].positionX)
+# addStation()
+# updateDF()
 
+# status = True
+# while status == True:
 
+print("\n\n\n\n\n\n What do you want to do?")
+input = input("View info of one station = 1, Add station = 2, Quit = 3")
+if input == 1:
+   
+
+  
